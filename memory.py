c09ad2e5d4139307c6783395f72ca6b1c76a4916
vorobiov-alexandr <78603656+vorobiov-alexandr@users.noreply.github.com>
2025-07-14 23:59:28 +0300

    Update memory.py

--- memory.py
+++ memory.py
@@ -1,8 +1,9 @@
 import json
+
 class Memory:
     def __init__(self):
         self.log = []
 
     def save_to_file(self, filename):
-        with open(filename, 'w', encoding='utf-8') as f:
+        with open(filename, "w", encoding="utf-8") as f:
             json.dump(self.log, f, ensure_ascii=False, indent=2)
